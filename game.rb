0cdc4f0e005b7daf6343f727bf383abfab3733e3
Matthew Berkompas <mtberkom@gmail.com>
2014-06-19 03:11:35 +0000

    Improved version

--- game.rb
+++ game.rb
@@ -1,9 +1,9 @@
 #!usr/bin/ruby
-
+  
 # There are two ways to print text to the screen in Ruby.
 #
-# puts  - Prints out the text AND adds a new line (\n) to the end, so that the
-#         next thing you puts will be on a new line.
+# typewriter  - Prints out the text AND adds a new line (\n) to the end, so that the
+#         next thing you typewriter will be on a new line.
 #
 # print - Prints out the text and DOES NOT put a new line (\n) at the end. This
 #         means that when you print, the next print will put stuff on the same
@@ -11,8 +11,8 @@
 #
 # For example:
 #
-#   puts "Hello"
-#   puts "There"
+#   typewriter "Hello"
+#   typewriter "There"
 #
 # Prints out this:
 #
@@ -36,44 +36,84 @@ def typewriter(text) # Name the method "Typewriter"
   # one until we're done.
   text.each_char do |letter|
       print letter
-      sleep 0.05 # Wait 5/100 of a second to print the next character.
+      sleep 0.01 # Wait 5/100 of a second to print the next character.
   end
-  print "\n" # Print a new line character to make this function act like puts.
+  print "\n" # Print a new line character to make this function act like typewriter.
 
   # Finally, return the text. In ruby, you don't have to type "return"
   # the last thing is automatically returned
   text
 end
 
+def abort_if(answer, wrong_answer, abort_message)
+  if answer == "#{wrong_answer}\n" # Add in the \n stuff so we don't have to type every time
+      abort abort_message
+  end
+end
+
+
 # I can now use this function two ways:
 #
 # 1. With parentheses: typewriter("Words to type")
 # 2. Or without parentheses: typewriter "Words to type"
-puts "STAR WARS!!!!"
+typewriter "STAR WARS!!!!"
 typewriter %{You are a smuggler. Your crew has betrayed you, but still has enough of a
 soft spot that they are not killing you. Instead, they decided that they would
 abandon you on a desert moon in the outer rim, and give you 3 items from
+
 your locker. Which items do you take?}
 
-typewriter "1) Electronic Lock-Pick"
-typewriter "2) Blaster"
-typewriter "3) Concussion Rifle"
-typewriter "4) Thermal Detonator"
-typewriter "5) Personal Shield Generator"
-typewriter "6) Grappling gun"
+   typewriter "1) Electronic Lock-Pick"
+   typewriter "2) Blaster"
+   typewriter "3) Concussion Rifle"
+   typewriter "4) Thermal Detonator"
+   typewriter "5) Titanium Reinforced armor"
+   typewriter "6) Grappling gun"
 
 sleep 0.5 # Wait half a second before giving them the option to select stuff.
 
-typewriter "Separate your selections with a comma:"
-print ">"
+  typewriter "Separate your selections with a comma:"
+  print ">"
 
-stuff = []
+STUFF = []
 answers = gets.gsub("\n", "").split(",")
 
 # I can chain functions together.
 # This is the same as abort(typewriter("Some words"))
 abort typewriter(%{Your crew was surprised that you didn't listen to them when they said
 you could only have 3 items. They shot you dead.}) if answers.size > 3
+
+class Question
+  def initialize(text, options)
+    @text = text
+    @options = options
+  end
+  
+  def display!
+    typewriter @text
+    
+    @options.each_with_index do |option, index|
+      if option[:required_stuff].nil? || ::STUFF.include?(option[:required_stuff])
+        option[:number] = index + 1
+        typewriter "#{option[:number]}) #{option[:text]}"
+      end
+    end
+    print ">"
+    
+    answer = gets.to_i
+    selected_option = @options.find { |o| o[:number] == answer }
+
+    if selected_option.nil?
+      puts "You selected an invalid option."
+      display!
+    else
+      if selected_option[:correct] == false
+        typewriter(selected_option[:death] || "You died.")
+        abort
+      end
+    end
+  end
+end
 
 # This is one way to add stuff to an array in Ruby.
 #
@@ -86,86 +126,82 @@ you could only have 3 items. They shot you dead.}) if answers.size > 3
 #   array = []
 #   array.push(:lock_pick)
 #   array # => [:lock_pick]
-stuff << :lock_pick           if answers.include?("1")
-stuff << :blaster             if answers.include?("2")
-stuff << :concussion_rifle    if answers.include?("3")
-stuff << :thermal_detonator   if answers.include?("4")
-stuff << :shield_generator    if answers.include?("5")
-stuff << :grappling_gun       if answers.include?("6")
+STUFF << :lock_pick           if answers.include?("1")
+STUFF << :blaster             if answers.include?("2")
+STUFF << :concussion_rifle    if answers.include?("3")
+STUFF << :thermal_detonator   if answers.include?("4")
+STUFF << :shield_generator    if answers.include?("5")
+STUFF << :grappling_gun       if answers.include?("6")
 
 # Include variables in strings like this:
-puts "You have this stuff: #{stuff}"
+typewriter "You have this stuff: #{STUFF}"
 
-puts "--------------------------------------------"
-puts "CHAPTER 1"
-puts "In which you search for a way off this rock."
-puts "--------------------------------------------"
-puts %{You find yourself in an abandoned village, on a narrow, one-way street.
-  Which way are going to go?}
+typewriter "--------------------------------------------"
+typewriter "CHAPTER 1"
+typewriter "In which you search for a way off this rock."
+typewriter "--------------------------------------------"
 
-  puts "1) Go up the street."
-  puts "2) Go down the street."
+#First Question
+Question.new("You find yourself in an abandoned village, on a narrow, one-way street. Which way are going to go?", [{
+  text: "Go up the street.",
+  correct: false,
+  death: "You are shot from out of nowhere by a roaming band of Tusken Raiders. Bye Bye."
+}, {
+  text: "Go down the street.",
+  correct: true
+}]).display!
 
-print ">"
+#Second Questions
+Question.new("You walk down the street and find yourself in front of a warehouse. Do you want to go in or continue of your way?", [{
+  text: "Go into the warehouse.",
+  correct: true
+}, {
+  text: "Continue on your way.",
+  correct: false,
+  death: "You come around the building and are run over by a raging bantha! Start over :("
+}]).display!
 
-# Since we repeat this answer checking code a lot, wouldn't it be nice if
-# we could make it simpler? We can do just that with a custom function!
+#Third Question
+Question.new("You see that the Warehouse is covered in junk and debris but in the back corner you see some sort of spacecraft and investigate it.", [{
+  text: "Cut the green wire.",
+  correct: false,  
+  death: "The ship blew up and you died."
+},{
+  text: "Cut the red wire.",
+  correct: true
+}]).display!
 
-def abort_if(answer, wrong_answer, abort_message)
-  if answer == "#{wrong_answer}\n" # Add in the \n stuff so we don't have to type every time
-      abort abort_message
-  end
-end
+#Fourth Question
+Question.new("The ship powered up and you open the sliding door. Just outside the door, 6 battle droids are standing outside ready to shoot. Do you want to,", [{
+  text: 'Shoot them with the space ship.',
+  correct: false,
+  death: "You couldn't reach the ship in time. They shoot you in the back."
+}, {
+  text: "Hide behind the wall.",
+  correct: true
+}]).display!
 
-answer = gets
-abort_if answer, 1, "You are shot from out of nowhere by a roaming band of Tusken Raiders. Bye Bye."
-
-puts "You walk down the street and find yourself in front of a warehouse.
- Do you want to go in or continue of your way?"
-
-  puts "1) Go into the warehouse."
-  puts "2) Continue on your way."
-
-print ">"
-answer = gets
-abort_if answer, 2, %{You come around the building and are run over by a raging bantha! Start over :(}
-
-puts "You see that the Warehouse is covered in junk and debris but in the back corner
-you see some sort of aircraft and investigate it."
-
-puts "1) Cut the green wire."
-puts "2) Cut the red wire."
-print ">"
-
-answer = gets
-abort_if answer, 1, "The ship blew up and you died."
-
-puts "The ship powered up and you open the sliding door. Just outside the door, 6 battle droids 
-are standing outside ready to shoot. Do you want to,"
-puts "1) Shoot them with the space ship."
-puts "2) Hide behind the wall."
-print ">"
-
-answer = gets
-abort_if answer, 1, "You couldn't reach the ship in time. They shoot you in the back."
-
-puts "The droids come around the corner and surround you. You can,"
-puts "1) Burst through them and try to escape in the ship."
-puts "2) Shoot them with your blaster."
-puts "3) Surrender."
-print ">"
-
-answer = gets
-abort_if answer, 2, "Some droids stayed outside and shoot you as you pass them. Dead again!"
-
-answer = gets
-abort_if answer, 3, "Something goes wrong at the droid control center. They go against the code and detonate the entire building."
-
-puts "You successfully elude their fire and shut the cockpit. Do you want to"
-puts"1) Shoot them with your cannons."
-puts"2) Fly away immediately."
-print ">"
-
-answer = gets
-abort_if answer, 1, "You used up the little power you had and were unable to escape the blow you and the ship sky high. MISSION FAILED!"
-abort_if answer, 2, "You barely escape with your life. Congradulations! You have successfully gotten off this rock!"
+Question.new("The droids come around the corner and surround you. You can,", [{
+  text: "Burst through them and try to escape in the ship.",
+  correct: true
+}, {
+  text: "Surrender.",
+  correct: false,
+  death: "Something goes wrong at the droid control center. They go against the code and detonate the entire building."
+}, {
+  text: "Shoot them with your blaster.",
+  correct: false,
+  required_stuff: :blaster,
+  death: "Some droids stayed outside and shoot you as you pass them. Dead again!"
+}]).display!
+  
+Question.new("You successfully elude their fire and shut the cockpit. Do you want to", [{
+  text: "Shoot them with your cannons.",
+  correct: false,
+  death: "You used up the little power you had and were unable to escape. They blow you and the ship sky high. MISSION FAILED!"
+}, {
+  text: "Fly away immediately.",
+  correct: true
+}]).display!
+  
+puts "You barely escape with your life. Congratulations! You have successfully gotten off this rock!"
